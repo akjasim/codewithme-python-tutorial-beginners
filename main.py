67f6c172e7358d811946448ae5f5fe8b31c2aac6
akjasim <jasimjasim786@gmail.com>
2020-04-23 11:34:35 +0530

    done

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
-# Loops - while loop
-
-# print nos 1 to 6
-
-i = 1
-while i < 7:
-    print(i)  # 1, 2, 3,   6
-    i = i + 1
+# Functions
 
 
-# break
-i = 1
-while i < 7:
-    if i == 3:
-        break
-    print(i)  # 1, 2, 3,   6
-    i = i + 1
+def add(a, b):
+    return a+b
 
-# continue
-i = 0
-while i < 6:
-    i = i + 1
-    if i == 3:
-        continue
-    print(i)
 
-# else
-i = 1
-while i < 7:
-    # if i == 3:
-    #     break
-    print(i)
-    i = i + 1
-else:
-    print('while completed successfully.')
+print(add(4, 6))
+
+print(add(5, 11))
+
+print(add(2, 2))
